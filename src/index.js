fe128041437efb5ce2de9afe7a42419d78bd477f
Vovandir <106840463+Vovandir@users.noreply.github.com>
2022-11-06 21:42:52 +0200

    c

--- src/index.js
+++ src/index.js
@@ -9,29 +9,32 @@ const input = getEl('input');
 const form = getEl('.search-form');
 const gallery = getEl('.gallery');
 const loadMoreButton = getEl('.load-more');
-let page = 1;
-let amountOfImages = 0;
-let loadedImages = 0;
+let pageAmount = 1;
+
+
+document.querySelectorAll('.header-heading-letters').forEach(el => el.style.color = getRandomHexColor());
 
 const submitFunction=()=> {
   event.preventDefault();
   gallery.innerHTML = ''; 
   page = 1;
-  mamaFunction(); 
-  console.log(amountOfImages,loadedImages);
+  mamaFunction().then(result => {
+    pageAmount = Math.floor(result.totalHits / 40);
+    console.log(pageAmount);
+    if (page > pageAmount) { loadMoreButton.classList.add('hidden') };
+    if (result.totalHits > 0) { Notiflix.Notify.success(`Hooray! We found ${result.totalHits} images.`) };    
+    if (result.totalHits > 40) { loadMoreButton.classList.remove('hidden') };
+  }).catch(error => console.error(error));
 }
 
 const loadMoreFunction = () => {
   page += 1;
-  mamaFunction();
-
-  if (loadedImages >= amountOfImages) {
-    loadMoreButton.classList.add('hidden')
-  }
-   
+  if (page > pageAmount) { loadMoreButton.classList.add('hidden');Notiflix.Notify.info("We're sorry, but you've reached the end of search results.") };
+  mamaFunction();   
 }
 function mamaFunction() {
-  fetchImage(input.value).then(image => renderImages(image)).then((result)=>console.log(result)).catch((error) => console.error(error));
+   return fetchImage(input.value).then(image => renderImages(image));
+  
  
   
 }
@@ -53,38 +56,37 @@ function getRandomHexColor() {
 }
 
 function renderImages(image) {
-  if (image.totalHits > 0) {
-    
-    loadMoreButton.classList.remove('hidden');
-    Notiflix.Notify.success(`Hooray! We found ${image.totalHits} images.`)
-    const markup = image.hits.map((im) => `<a class='image-card' style="background-color:${getRandomHexColor()};color:${getRandomHexColor()}" href="${im.largeImageURL}">
+  if (image.totalHits > 0) {   
+   
+    const markup = image.hits
+      .map((im) => `<a class='image-card' style="background-color:${getRandomHexColor()}" href="${im.largeImageURL}">
    <img class='img' src='${im.webformatURL}' loading="lazy"title="${im.tags}" alt='${im.tags}' width='260' height='200'>
    <div class="text-content">
     <div class="text-elements">
-      <p class="heading">likes</p> 
-      <p class="text">${im.likes}</p>
+      <p style="color:${getRandomHexColor()}" class="heading">likes</p> 
+      <p style="color:${getRandomHexColor()}" class="text">${im.likes}</p>
     </div>
     <div class="text-elements">
-      <p class="heading">views</p> 
-      <p class="text">${im.views}</p>
+      <p style="color:${getRandomHexColor()}"  class="heading">views</p> 
+      <p style="color:${getRandomHexColor()}"  class="text">${im.views}</p>
     </div>
     <div class="text-elements">
-      <p class="heading">comments</p> 
-      <p class="text">${im.comments}</p>
+      <p style="color:${getRandomHexColor()}"  class="heading">comments</p> 
+      <p style="color:${getRandomHexColor()}"  class="text">${im.comments}</p>
     </div>
     <div class="text-elements">
-      <p class="heading">downloads</p> 
-      <p class="text">${im.downloads}</p>
+      <p style="color:${getRandomHexColor()}"  class="heading">downloads</p> 
+      <p style="color:${getRandomHexColor()}"  class="text">${im.downloads}</p>
     </div>  
   </div>
-   </a>
-  `).join('');
+   </a>`).join('');
   gallery.insertAdjacentHTML('beforeend', markup);
   new SimpleLightbox('.gallery a', {
 	captionDelay: 250
   })
   }
-  else { loadMoreButton.classList.add('hidden'); Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."); }
+  else { loadMoreButton.classList.add('hidden'); Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.") }
+  return image
    
 }
 
